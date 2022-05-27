e899597b7cf2fb0bcc6e42da32032754b449dacf
heatherwatson96 <heatherbell96@icloud.com>
2022-05-27 17:01:06 +0100

    changes to layout and background pic loaded from pexels using new api

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 let currentDate = new Date();
 
 function getDate(date) {
-  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
   let day = days[date.getDay()];
   let hour = date.getHours();
   let minutes = date.getMinutes();
@@ -16,51 +25,43 @@ function getDate(date) {
 let liveDateElement = document.querySelector("#live-date");
 liveDateElement.innerHTML = getDate(currentDate);
 
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-
-  return days[day];
-}
-
 function displayForecast(response) {
   let forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
 
-  let forecastHTML = `<div class="row">`;
+  let forecastHTML = `<div class="row days-container">`;
 
   forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
+    if (index < 5) {
       forecastHTML =
         forecastHTML +
         `
-    <div class="col-2">
-      <div class="weather-forecast-date">${formatDay(
+      <div class="col day-background days all-days-container">
+      <div class="weather-forecast-date" >${formatDay(
         forecastDay.dt
       )}</div>${index}
-      <img 
-      src="https://openweathermap.org/img/wn/${
-        forecastDay.weather[0].icon
-      }@2x.png" alt="" width="42"/>
-      <div class="weather-forecast-temperature">
-
-      
-      <span class="weather-forecast-temp-min">${Math.round(
-        forecastDay.temp.min
-      )}° |</span>
+        <img 
+        src="https://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" alt="" width="42"/>
+        <div class="weather-forecast-temperature">
         
         
-     <span class="weather-forecast-temp-max">${Math.round(
-       forecastDay.temp.max
-     )}°</span>
-      </div>
-    </div>
-    `;
+        <span class="weather-forecast-temp-min">${Math.round(
+          forecastDay.temp.min
+        )}° |</span>
+          
+          
+          <span class="weather-forecast-temp-max">${Math.round(
+            forecastDay.temp.max
+          )}°</span>
+            </div>
+            </div>
+            `;
+      console.log(response.data);
     }
   });
-
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
@@ -70,10 +71,17 @@ function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  return days[day];
+}
 function showTemp(response) {
   let place = response.data.name;
   let placeElement = document.querySelector("#place");
   placeElement.innerHTML = `${place}`;
+
   let currentWeather = response.data.weather[0].main;
   let currentWeatherElement = document.querySelector("#current-weather");
   currentWeatherElement.innerHTML = `${currentWeather}`;
@@ -84,7 +92,7 @@ function showTemp(response) {
 
   let wind = Math.round(response.data.wind.speed);
   let windElement = document.querySelector("#wind");
-  windElement.innerHTML = `${wind} mps`;
+  windElement.innerHTML = `${wind} <small>m/ps</small>`;
 
   celciusTemp = response.data.main.temp;
   let tempElement = document.querySelector("#temp");
@@ -99,16 +107,37 @@ function showTemp(response) {
   getForecast(response.data.coord);
 }
 
-function searchCity(cityInput) {
-  let cityInputCorrections = cityInput.trim().toUpperCase();
+function searchCity(cityInputName) {
+  let cityInputCorrections = cityInputName.trim().toUpperCase();
   let apiKey = "6089e4fb28464b6a73284bc65d80e7ed";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInputCorrections}&units=metric`;
   axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemp);
 }
+function handlePexelsResponse(response) {
+  console.log(response.data.photos[0].src.portrait);
 
+  let locationTimeBackgroundElement = document.querySelector(
+    "#location-time-background"
+  );
+
+  locationTimeBackgroundElement.style.backgroundImage = `url("${response.data.photos[0].src.portrait}")`;
+}
+function getCityPic(cityName) {
+  let pexelsApiKey = "563492ad6f9170000100000104663ae2c247449f800144770696fdb3";
+  let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${cityName}&orientation=portrait`;
+
+  axios
+    .get(`${pexelsApiUrl}`, {
+      headers: {
+        Authorization: pexelsApiKey,
+      },
+    })
+    .then(handlePexelsResponse);
+}
 function onSubmit(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
+  getCityPic(cityInput.value);
   searchCity(cityInput.value);
 }
 
@@ -135,3 +164,4 @@ let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", displayCelciusTemperature);
 
 searchCity("Glasgow");
+getCityPic("Mountains night");
